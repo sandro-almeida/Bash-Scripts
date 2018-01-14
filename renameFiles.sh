4ad5d5a3ea9a88608e5661af81c4cc3029a8babd
Sandro Almeida <sandro001@gmail.com>
2018-01-14 10:08:23 -0200

    Add third argument and other validations

--- renameFiles.sh
+++ renameFiles.sh
@@ -1,17 +1,37 @@
+###############################################################################
+# renameFiles.sh
+# Purpose: rename all files from a folder using a new pattern name. A number
+#          starting from 1 will be used as a suffix for the renamed files.
+#
+# Usage: $0 '<OriginDirectory>' <NewFilePatternName> <ApplyChanges>
+# 
+# Arguments:
+#  <OriginDirectory>: name of the folder where the files will be renamed.
+#  <NewFilePatternName>: new pattern name to be applied to the files.
+#  <ApplyChanges>: if set to 'S', then the changes will be applied, otherwise
+#     only indicates the changes to be made but not apply them.
+###############################################################################
+
 #Read the arguments
-if [ $# -ne 2 ] 
+if [ $# -ne 3 ] 
 then
-    echo "Number of arguments required is 2"
-    echo "Expected arguments: $0 <OriginDirectory> <NewFilePatternName>"
+    echo "Number of arguments required is 3"
+    echo "Usage: $0 '<OriginDirectory>' <NewFilePatternName> <ApplyChanges>"
     exit 1
 fi
 
-TEMP_DIR=./temp
 ORIGIN_DIR=$1
 FILE_PATTERN=$2
+APPLY_CHANGES=$3
+
+NOW=$(date +'%Y%m%d%H%M')
+TEMP_DIR=./temp-${NOW}
+TEMP_FILE=${NOW}-tempFile.txt
+
 I=0
 echo "Origin directory is: [${ORIGIN_DIR}]"
 echo "File pattern name is: [${FILE_PATTERN}]"
+echo "Apply changes? [${APPLY_CHANGES}]"
 
 #Validate origin directory
 if [ ! -d ${ORIGIN_DIR} ]
@@ -20,18 +40,21 @@ then
     exit 1
 fi
 
+cd ${ORIGIN_DIR}
+pwd
+
 #Create a temp dir
 if [ ! -d ${TEMP_DIR} ] 
 then
     mkdir ${TEMP_DIR}
+else
+    echo "Temporary directory ${TEMP_DIR} has to be created but it already exists. Please try again in a minute."
+    exit 1
 fi
 
-cd ${ORIGIN_DIR}
-pwd
-
 #List all files, except directories
-ls -p | grep -v / > ${TEMP_DIR}/tempFiles.txt
-NUMBER_OF_FILES=$(cat ${TEMP_DIR}/tempFiles.txt | wc -l)
+ls -p | grep -v / > ${TEMP_DIR}/${TEMP_FILE}
+NUMBER_OF_FILES=$(cat ${TEMP_DIR}/${TEMP_FILE} | wc -l)
 SUFFIX_LENGTH=${#NUMBER_OF_FILES}
 echo "Number of files listed is [${NUMBER_OF_FILES}]"
 echo "Suffix length is [${SUFFIX_LENGTH}]"
@@ -49,12 +72,20 @@ do
     SUFFIX=$(printf "%0${SUFFIX_LENGTH}.0f" ${I})
     NEW_NAME=${FILE_PATTERN}${SUFFIX}.${EXTENSION}
 
-    mv ${LINE} ${NEW_NAME}
+    if [ ${APPLY_CHANGES} == "S" ]
+    then
+        mv ${LINE} ${NEW_NAME}
+    fi
     echo "${LINE} -> ${NEW_NAME}"
 
-done < ${TEMP_DIR}/tempFiles.txt
+done < ${TEMP_DIR}/${TEMP_FILE}
 
-rm ${TEMP_DIR}/tempFiles.txt
+if [ ${APPLY_CHANGES} != "S" ]
+then
+    echo "Changes NOT applied !!!"
+fi
+
+rm ${TEMP_DIR}/${TEMP_FILE}
 rmdir ${TEMP_DIR}
 echo "Temp files and directory removed"
 
