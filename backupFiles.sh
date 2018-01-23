b43632982150d079aa61b4736ffd3eab73738064
Sandro Almeida <sandro001@gmail.com>
2018-01-23 19:32:11 -0200

    Add backup validation

--- backupFiles.sh
+++ backupFiles.sh
@@ -1,7 +1,9 @@
 ###############################################################################
 # backupFiles.sh
 # Purpose: backup files and folders from origin to destination. Only either
-#          new files or changed ones are moved.
+#          new files or changed ones are moved. It does not synchronize
+#          origin and destination. It validates if the backup was
+#          successfully completed.
 #
 # Usage: $0 '<OriginDirectory>' '<DestinationDirectory>' <ApplyIt>
 # 
@@ -10,13 +12,11 @@
 #     copied.
 #  <DestinationDirectory>: name of the destination folder TO where the files
 #     will be backed-up.
-#  <ApplyIt>: if set to 'S', then the backup will be applied, otherwise
+#  <ApplyIt>: if set to 'Y', then the backup will be applied, otherwise
 #     only indicates the backup to be made but not apply them.
 ###############################################################################
 
-#TODO
-#> Turn the third argument functional
-#> Compare origin and destination after the backup and print a message
+echo "Starting script ${0} at $(date +'%Y-%m-%d %H:%M:%S.%3N')"
 
 #Read the arguments
 if [ $# -ne 3 ] 
@@ -91,7 +91,10 @@ do
     then
         if [ ! -d ${DESTINATION_ABS_DIR}/${LINE_CONTENT} ]
         then
-            mkdir ${DESTINATION_ABS_DIR}/${LINE_CONTENT}
+            if [ ${APPLY_IT} == "Y" ]
+            then
+                mkdir ${DESTINATION_ABS_DIR}/${LINE_CONTENT}
+            fi
             echo "New directory [${LINE_CONTENT}] created at destination."
         fi
     else
@@ -99,14 +102,21 @@ do
     	if [ ! -f ${DESTINATION_ABS_DIR}/${LINE_CONTENT} ]
     	then
     	    #Copy new file to destination
-            cp -p ${LINE} ${DESTINATION_ABS_DIR}/${LINE_CONTENT%/*}
+    	    if [ ${APPLY_IT} == "Y" ]
+    	    then
+    	        cp -p ${LINE} ${DESTINATION_ABS_DIR}/${LINE_CONTENT%/*}
+    	    fi
             echo "New file [${LINE_CONTENT}] created at destination."
         else
         	if [ -f ${DESTINATION_ABS_DIR}/${LINE_CONTENT} ]
         	then
+        	    #Existing files are only copied if their timestamp differ
         	    if [ $(stat -c %Y ${LINE}) != $(stat -c %Y ${DESTINATION_ABS_DIR}/${LINE_CONTENT}) ]
         	    then
-        	        cp -fp ${LINE} ${DESTINATION_ABS_DIR}/${LINE_CONTENT%/*}
+        	        if [ ${APPLY_IT} == "Y" ]
+        	        then
+        	            cp -fp ${LINE} ${DESTINATION_ABS_DIR}/${LINE_CONTENT%/*}
+        	        fi
                     echo "Existing file [${LINE_CONTENT}] updated at destination."
                 fi
             fi
@@ -114,6 +124,63 @@ do
     fi
 done < ${TEMP_ABS_DIR}/${TEMP_FILE}
 
+if [ ${APPLY_IT} != "Y" ]
+then
+    echo "Backup was NOT applied !!! It was ONLY INFORMATIONAL."
+fi
+
+BKP_STATUS="OK"
+
+#Compare origin and destination after backup applied
+if [ ${APPLY_IT} == "Y" ]
+then
+    COMP_ORIG_DEST="OK"
+    while read LINE
+	do 
+	    #Remove the first two characters if they match './'
+	    if [ ${LINE:0:2} == "./" ]
+	    then
+	        LINE_CONTENT=${LINE:2}
+	    else
+	    	LINE_CONTENT=${LINE}
+	    fi
+
+	    #Verify if it is a directory and exists at destination
+	    if [ -d ${LINE} ]
+	    then
+	        if [ ! -d ${DESTINATION_ABS_DIR}/${LINE_CONTENT} ]
+	        then
+	            echo "Directory [${LINE_CONTENT}] was NOT created at destination."
+	            COMP_ORIG_DEST="FAIL"
+	        fi
+	    else
+	    	#Verify if it is a file and exists at destination
+	    	if [ ! -f ${DESTINATION_ABS_DIR}/${LINE_CONTENT} ]
+	    	then
+	    	    echo "File [${LINE_CONTENT}] was NOT created at destination."
+	    	    COMP_ORIG_DEST="FAIL"
+	    	fi
+	    fi
+	done < ${TEMP_ABS_DIR}/${TEMP_FILE}
+
+    if [ ${COMP_ORIG_DEST} == "FAIL" ]
+    then
+        echo "Backup FAILED: some directories/files were not copied !!! Please verify."
+        BKP_STATUS="FAIL"
+    else
+    	echo "Backup SUCCESSFULLY completed !!!"
+    fi
+fi
+
 rm ${DESTINATION_ABS_DIR}/${TEMP_DIR}/${TEMP_FILE}
 rmdir ${DESTINATION_ABS_DIR}/${TEMP_DIR}
 echo "Temp file and directory removed"
+
+echo "Finishing script ${0} at $(date +'%Y-%m-%d %H:%M:%S.%3N')"
+
+if [ ${BKP_STATUS} == "FAIL" ]
+then
+    exit 2
+fi
+
+exit 0
